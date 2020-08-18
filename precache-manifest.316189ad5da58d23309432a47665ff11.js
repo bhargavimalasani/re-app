a239e8420f6bfdd7089f4fdbcb682bb2801ecfa0
System Administrator <root@macbook-pro.home>
2020-08-18 15:35:41 -0400

    Updates

---
+++ precache-manifest.316189ad5da58d23309432a47665ff11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50acd2dcc163638cb07eb34ecbb24db6",
+    "url": "/re-app/index.html"
+  },
+  {
+    "revision": "4b7d281428dfcc4774dc",
+    "url": "/re-app/static/css/main.e28e0e1b.chunk.css"
+  },
+  {
+    "revision": "cadaead9e77247564c76",
+    "url": "/re-app/static/js/2.4c910acf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/re-app/static/js/2.4c910acf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b7d281428dfcc4774dc",
+    "url": "/re-app/static/js/main.c62e4775.chunk.js"
+  },
+  {
+    "revision": "564f9e86e1ad91972367",
+    "url": "/re-app/static/js/runtime-main.d5a01a4c.js"
+  }
+]);
